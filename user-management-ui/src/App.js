317add27781000c7201395a54ffe65cf80f52b01
Ioan Cioclut <ioan.cioclut@gmail.com>
2020-05-03 21:29:48 +0300

    Implement health check from UI

--- user-management-ui/src/App.js
+++ user-management-ui/src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to react.
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isAlive: false}
+    }
+
+    componentDidMount() {
+        fetch('  http://localhost:3001/healthcheck').then(response => {
+            console.log(response.status);
+            if (response.status === 200) {
+                this.setState({isAlive: true});
+            }
+        });
+    }
+
+    render() {
+        if (this.state.isAlive) {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <p>
+                            Welcome to User management.
+                        </p>
+                        <img src={logo} className="App-logo" alt="logo"/>
+                    </header>
+                </div>
+            );
+        } else {
+            return (
+                <div className="App">
+                    <header className="App-header"><p>
+                        User management is not available :(
+                    </p>
+                    </header>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
