bcc684978375490ca5fe64efb4572fd9db7df189
Ioan Cioclut <ioan.cioclut@gmail.com>
2020-05-05 00:27:57 +0300

    Add rest api for creating a new user. Integrate with postgress

--- UserManagement/routes/users.js
+++ UserManagement/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const {User} = require('../db/connection');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+    User.findAll().then(result => {
+        res.status(200).send(result);
+    });
+});
+
+/* Create new user. */
+router.post('/', (req, res, next) => {
+    User.create(req.body).then(user => {
+        res.status(201).send(user);
+    }).catch(err=>next(err));
 });
 
 module.exports = router;
